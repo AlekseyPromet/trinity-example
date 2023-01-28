d653c6b5e65dfc82160954c3b197533f9634b5b8
Alexey Perminov <promet.alex@gmail.com>
2023-01-28 15:07:13 +0500

    add protobuf for  GRPC service

--- migrations/sql/orders/1.Orders.up.sql
+++ migrations/sql/orders/1.Orders.up.sql
@@ -1,23 +1,28 @@
 CREATE TABLE IF NOT EXISTS  public.orders (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  customer_id UUID REFERENCES public.customer (id),
-  bucket_goods_id UUID REFERENCES public.bucket_goods (id),
-  
-  created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP WITHOUT TIME ZONE,
-  is_deleted BOOLEAN NOT NULL DEFAULT FALSE
+  id              UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  customer_id     UUID REFERENCES public.customer (id),
+  bucket_goods_id UUID REFERENCES public.bucket_goods (id),  
+  created_at      TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at      TIMESTAMP WITHOUT TIME ZONE,
+  is_deleted      BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 -- For many2many order and goods
 CREATE TABLE IF NOT EXISTS public.bucket_goods (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  good_id UUID REFERENCES public.goods (id),
+  id             UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  order          UUID REFERENCES public.orders (id), 
+  good_id_1      UUID REFERENCES public.goods (id),
+  good_id_2      UUID REFERENCES public.goods (id),
+  good_id_3      UUID REFERENCES public.goods (id),
+  good_id_4      UUID REFERENCES public.goods (id),
+  good_id_5      UUID REFERENCES public.goods (id),
+  next_bucket_id UUID REFERENCES public.bucket_goods
 );
 
 CREATE TABLE IF NOT EXISTS public.orders_history (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   order       UUID REFERENCES public.orders (id), 
-  created_at  TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
+  created_at  TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   rating      INT DEFAULT 1,
   CONSTRAINT  order_rating CASE (rating >= 0 AND rating <= 5)
 )
